5e09239ee2751aff61fc672c70afc940517ad660
Scripted Logic <ouassbader184@gmail.com>
2025-07-03 17:16:06 +0100

    feat: Add Deliveries component and integrate into SellerPage; update package dependencies

--- frontend/src/pages/SellerPage.tsx
+++ frontend/src/pages/SellerPage.tsx
@@ -2,6 +2,7 @@ import { SidebarProvider } from '../components/Shadcn/Sidebar';
 import { SellerSidebar } from '../components/SellerPage/Sidebar';
 import { MainLayout } from '../components/layouts/MainLayout';
 import Overview from '@/components/SellerPage/Overview';
+import Deliveries from '@/components/SellerPage/Deliveries';
 import { Shops } from '../components/SellerPage';
 import ProfilePage from './ProfilePage';
 import { Routes, Route } from 'react-router-dom';
@@ -16,7 +17,7 @@ const SellerPage = () => {
             <Route index element={<Overview />} />
             <Route path="products" element={<div className="p-6">Mes produits - Coming Soon</div>} />
             <Route path="store" element={<Shops />} />
-            <Route path="shipping" element={<div className="p-6">Services de livraison - Coming Soon</div>} />
+            <Route path="shipping" element={<Deliveries />} />
             <Route path="orders" element={<div className="p-6">Commandes - Coming Soon</div>} />
             <Route path="inbox" element={<div className="p-6">Chats - Coming Soon</div>} />
             <Route path="boost" element={<div className="p-6">Boostez votre entreprise - Coming Soon</div>} />
